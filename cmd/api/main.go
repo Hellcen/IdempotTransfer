d3e8718c26c3ea223d8336581507bfebd9c6c210
amir <gumerovamihan@gmail.com>
2026-02-27 23:05:20 +0300

    parsing yaml file

---
+++ cmd/api/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	// "fmt"
+	"idempot/internal/config"
+	"log"
+)
+
+func main(){
+	config, err := config.Load()
+	if err != nil {
+        log.Fatal("failed to load configuration:", err)
+    }
+
+	// fmt.Println(config)
+}
---
+++ internal/config/config.go
@@ -0,0 +1,65 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Server ServerConfig `yaml:"Server"`
+	DB     DBConfig     `yaml:"DB"`
+	Token  TokenConfig  `yaml:"Token"`
+	Logger LoggerConfig `yaml:"Logger"`
+}
+
+type ServerConfig struct {
+	Port string `yaml:"port" default:"8080"`
+}
+
+type DBConfig struct {
+	DatabaseURL        string        `yaml:"databaseURL"`
+	Port               int           `yaml:"port" default:"5432"`
+	MaxOpenConnection  int           `yaml:"maxOpenConn" default:"15"`
+	MaxIdleConnection  int           `yaml:"maxIdleConn" default:"10"`
+	ConnectionLifetime time.Duration `yaml:"connLifetime" default:"3600s"`
+}
+
+type TokenConfig struct {
+	AuthToken string `yaml:"authToken" default:"test-token"`
+}
+
+type LoggerConfig struct {
+	LoggerLevel string `yaml:"loggerLevel" default:"info"`
+}
+
+func Load() (*Config, error) {
+	viper.AutomaticEnv()
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AddConfigPath("./internal/config")
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			log.Println("file not found")
+		} else {
+			log.Println("error reading config file")
+		}
+	} else {
+		log.Printf("using config file: %s", viper.ConfigFileUsed())
+	}
+
+	log.Printf("all settings: %v", viper.AllSettings())
+
+	var config Config
+
+	if err := viper.Unmarshal(&config); err != nil {
+		return nil, fmt.Errorf("unable to decode into struct: %w", err)
+	}
+
+	return &config, nil
+}
